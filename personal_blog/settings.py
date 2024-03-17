f6c80d2a55416c9daf44ff770df4fb54595157d3
khoapd <khoa.phamdangaio97@gmail.com>
2024-03-17 20:06:45 +0700

    feat: update connect to db sqlite3

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -131,18 +131,25 @@ WSGI_APPLICATION = "personal_blog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.postgresql',
+# 		'NAME': 'postgres',
+# 		'USER': 'postgres',
+# 		'PASSWORD': 'admin',
+# 		'HOST': 'localhost',
+# 		'PORT': '5432',
+# 		'OPTIONS': {
+# 			'options': '-c search_path=public'
+# 		},
+# 	}
+# }
+
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql',
-		'NAME': 'postgres',
-		'USER': 'postgres',
-		'PASSWORD': 'admin',
-		'HOST': 'localhost',
-		'PORT': '5432',
-		'OPTIONS': {
-			'options': '-c search_path=public'
-		},
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
